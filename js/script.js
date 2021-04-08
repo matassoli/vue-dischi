2f38a5e75f408a0ab78ce8c7077d44099bc1c4d9
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-04-08 23:55:21 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 var app = new Vue({
   el: "#app",
   data: {
-    albums: []
+    albums: [],
+    genre: ["All"],
+    genreActive: "All"
+  },
 
+
+  methods: {
+    filter: function() {
+      this.albums.forEach((elemento) => {
+        if (this.genre.includes(elemento.genre) == false) {
+          this.genre.push(elemento.genre);
+        }
+      });
+    },
+
+    sort: function() {
+      return this.albums.sort((a, b) => a.year - b.year);
+    },
   },
 
   mounted: function() {
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((risposta) => {
-          this.albums = risposta.data.response;
-
-        }
-
-
-      );
-  }
-
-
-
+        this.albums = risposta.data.response;
+        this.sort();
+        this.filter();
+      });
+  },
 });
